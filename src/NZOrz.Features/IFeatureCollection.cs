49a163c1c4a5b98468ffada6fc234157afef4793
Victor Qu <Victor.X.Qu@newegg.com>
2025-02-08 17:29:56 +0800

    init

---
+++ src/NZOrz.Features/IFeatureCollection.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NZOrz.Features
+{
+    public interface IFeatureCollection
+    {
+        private const int DefaultInitialCapacity = 16;
+        private static volatile int typeLastIndex = -1;
+
+        private static class TypeSlot<T>
+        {
+            internal static readonly int Index = Interlocked.Increment(ref typeLastIndex);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static int GetIndex<TKey>() => TypeSlot<TKey>.Index;
+
+        internal static int RecommendedCapacity
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                var capacity = typeLastIndex + 1;
+
+                if (capacity < DefaultInitialCapacity)
+                {
+                    capacity = DefaultInitialCapacity;
+                }
+                else
+                {
+                    capacity <<= 1;
+                    if ((uint)capacity > (uint)Array.MaxLength)
+                        capacity = Array.MaxLength;
+                }
+
+                return capacity;
+            }
+        }
+
+        object? this[Type key] { get; set; }
+
+        TFeature? Get<TFeature>();
+
+        void Set<TFeature>(TFeature? instance);
+    }
+
+    public class FeatureCollection : IFeatureCollection
+    {
+        public object? this[Type key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public TFeature? Get<TFeature>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set<TFeature>(TFeature? instance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/NZOrz.Benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using NZOrz.Benchmarks;
+
+new TypeMapBenchmarks().Setup();
+new TypeMapBenchmarks().TypeMapLookupSetPool();
+var summary = BenchmarkRunner.Run<TypeMapBenchmarks>();
