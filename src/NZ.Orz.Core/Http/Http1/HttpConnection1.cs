84b893147080424d677f4a1cb31893517776a891
Victor Qu <Victor.X.Qu@newegg.com>
2025-03-25 17:30:16 +0800

    begin http1 ParseRequest

--- src/NZ.Orz.Core/Http/Http1/HttpConnection1.cs
+++ src/NZ.Orz.Core/Http/Http1/HttpConnection1.cs
@@ -15,19 +15,30 @@ namespace NZ.Orz.Http;
 
 public class HttpConnection1 : HttpProtocol
 {
+    private const byte ByteCR = (byte)'\r';
+    private const byte ByteLF = (byte)'\n';
+    private const byte ByteAsterisk = (byte)'*';
+    private const byte ByteForwardSlash = (byte)'/';
+    private const string Asterisk = "*";
+    private const string ForwardSlash = "/";
+
     private readonly ServiceContext serviceContext;
     private uint _requestCount;
     private volatile bool _requestTimedOut;
+    private ServerLimits limits;
+    private long _remainingRequestHeadersBytesAllowed;
 
     public HttpConnection1(ConnectionContext connectionContext, ServiceContext serviceContext, TimeoutControl timeoutControl) : base(GatewayProtocols.HTTP1, connectionContext)
     {
         Input = connectionContext.Transport.Input;
         this.serviceContext = serviceContext;
         TimeoutControl = timeoutControl;
+        this.limits = serviceContext.ServerOptions.Limits;
     }
 
     public PipeReader Input { get; }
     public TimeoutControl TimeoutControl { get; }
+
     public bool RequestTimedOut => _requestTimedOut;
 
     protected override bool BeginRead(out ValueTask<ReadResult> awaitable)
@@ -40,7 +51,7 @@ public class HttpConnection1 : HttpProtocol
     {
         Reset();
         _requestCount++;
-        TimeoutControl.SetTimeout(serviceContext.ServerOptions.Limits.KeepAliveTimeout, TimeoutReason.KeepAlive);
+        TimeoutControl.SetTimeout(limits.KeepAliveTimeout, TimeoutReason.KeepAlive);
     }
 
     protected override MessageBody CreateMessageBody()
@@ -65,7 +76,6 @@ public class HttpConnection1 : HttpProtocol
         catch (InvalidOperationException) when (_requestProcessingStatus == RequestProcessingStatus.ParsingHeaders)
         {
             throw BadHttpRequestException.GetException(RequestRejectionReason.MalformedRequestInvalidHeaders);
-            throw;
         }
         catch (BadHttpRequestException ex)
         {
@@ -129,7 +139,125 @@ public class HttpConnection1 : HttpProtocol
 
     private bool ParseRequest(ref SequenceReader<byte> reader)
     {
-        throw new NotImplementedException();
+        switch (_requestProcessingStatus)
+        {
+            case RequestProcessingStatus.RequestPending:
+                // Skip any empty lines (\r or \n) between requests.
+                // Peek first as a minor performance optimization; it's a quick inlined check.
+                if (reader.TryPeek(out byte b) && (b == ByteCR || b == ByteLF))
+                {
+                    reader.AdvancePastAny(ByteCR, ByteLF);
+                }
+
+                if (reader.End)
+                {
+                    break;
+                }
+
+                TimeoutControl.ResetTimeout(limits.RequestHeadersTimeout, TimeoutReason.RequestHeaders);
+
+                _requestProcessingStatus = RequestProcessingStatus.ParsingRequestLine;
+                goto case RequestProcessingStatus.ParsingRequestLine;
+            case RequestProcessingStatus.ParsingRequestLine:
+                if (TakeStartLine(ref reader))
+                {
+                    _requestProcessingStatus = RequestProcessingStatus.ParsingHeaders;
+                    goto case RequestProcessingStatus.ParsingHeaders;
+                }
+                else
+                {
+                    break;
+                }
+            case RequestProcessingStatus.ParsingHeaders:
+                if (TakeMessageHeaders(ref reader, trailers: false))
+                {
+                    _requestProcessingStatus = RequestProcessingStatus.AppStarted;
+                    // Consumed preamble
+                    return true;
+                }
+                break;
+        }
+
+        // Haven't completed consuming preamble
+        return false;
+    }
+
+    public bool TakeStartLine(ref SequenceReader<byte> reader)
+    {
+        // Make sure the buffer is limited
+        if (reader.Remaining >= limits.MaxRequestLineSize)
+        {
+            // Input oversize, cap amount checked
+            return TrimAndTakeStartLine(ref reader);
+        }
+
+        return ParseRequestLine(ref reader);
+
+        bool TrimAndTakeStartLine(ref SequenceReader<byte> reader)
+        {
+            var trimmedBuffer = reader.Sequence.Slice(reader.Position, limits.MaxRequestLineSize);
+            var trimmedReader = new SequenceReader<byte>(trimmedBuffer);
+
+            if (!ParseRequestLine(ref trimmedReader))
+            {
+                // We read the maximum allowed but didn't complete the start line.
+                throw BadHttpRequestException.GetException(RequestRejectionReason.RequestLineTooLong);
+            }
+
+            reader.Advance(trimmedReader.Consumed);
+            return true;
+        }
+    }
+
+    public bool TakeMessageHeaders(ref SequenceReader<byte> reader, bool trailers)
+    {
+        // Make sure the buffer is limited
+        if (reader.Remaining > _remainingRequestHeadersBytesAllowed)
+        {
+            // Input oversize, cap amount checked
+            return TrimAndTakeMessageHeaders(ref reader, trailers);
+        }
+
+        var alreadyConsumed = reader.Consumed;
+
+        try
+        {
+            var result = ParseHeaders(ref reader);
+            if (result)
+            {
+                TimeoutControl.CancelTimeout();
+            }
+
+            return result;
+        }
+        finally
+        {
+            _remainingRequestHeadersBytesAllowed -= reader.Consumed - alreadyConsumed;
+        }
+
+        bool TrimAndTakeMessageHeaders(ref SequenceReader<byte> reader, bool trailers)
+        {
+            var trimmedBuffer = reader.Sequence.Slice(reader.Position, _remainingRequestHeadersBytesAllowed);
+            var trimmedReader = new SequenceReader<byte>(trimmedBuffer);
+            try
+            {
+                if (!ParseHeaders(trailers, ref trimmedReader))
+                {
+                    // We read the maximum allowed but didn't complete the headers.
+                    throw BadHttpRequestException.GetException(RequestRejectionReason.HeadersExceedMaxTotalSize);
+                }
+
+                TimeoutControl.CancelTimeout();
+
+                reader.Advance(trimmedReader.Consumed);
+
+                return true;
+            }
+            finally
+            {
+                _remainingRequestHeadersBytesAllowed -= trimmedReader.Consumed;
+            }
+        }
     }
 
     internal override void DisableKeepAlive(ConnectionEndReason reason)
@@ -166,4 +294,13 @@ public class HttpConnection1 : HttpProtocol
     {
         throw new NotImplementedException();
     }
+
+    #region HttpParser
+
+    public bool ParseRequestLine(ref SequenceReader<byte> reader)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion HttpParser
 }
