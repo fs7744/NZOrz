0e488a80f19c8b4df451fad74f41a8d9b2e4643c
Victor Qu <Victor.X.Qu@newegg.com>
2025-03-26 17:26:29 +0800

    HttpHeaders

--- src/NZ.Orz.Core/Http/Abstractions/HttpHeaders.p.cs
+++ src/NZ.Orz.Core/Http/Abstractions/HttpHeaders.p.cs
@@ -21,7 +21,7 @@ public partial class HttpHeaders
     private void FastClear()
     {
         var r = _references;
-        r._ContentLength = StringValues.Empty;
+        r.CacheControl = StringValues.Empty;
     }
 
     private bool FastAdd(string k, StringValues value)
@@ -30,9 +30,9 @@ public partial class HttpHeaders
         switch (k.Length)
         {
             case 13:
-                if (ReferenceEquals(HeaderNames.ContentLength, k))
+                if (ReferenceEquals(HeaderNames.CacheControl, k))
                 {
-                    r._ContentLength = value;
+                    r.CacheControl = value;
                     return true;
                 }
 
@@ -51,9 +51,9 @@ public partial class HttpHeaders
             case 13:
                 if (ReferenceEquals(HeaderNames.ContentLength, k))
                 {
-                    if (!ReferenceEquals(StringValues.Empty, r._ContentLength))
+                    if (StringValues.Empty != r.CacheControl)
                     {
-                        r._ContentLength = StringValues.Empty;
+                        r.CacheControl = StringValues.Empty;
                         return true;
                     }
                     return false;
@@ -72,76 +72,73 @@ public partial class HttpHeaders
         switch (k.Length)
         {
             case 13:
-                if (ReferenceEquals(HeaderNames.ContentLength, k))
+                if (ReferenceEquals(HeaderNames.CacheControl, k))
                 {
-                    if (ReferenceEquals(StringValues.Empty, r._ContentLength))
+                    if (StringValues.Empty != r.CacheControl)
                     {
-                        return false;
-                    }
-                    else
-                    {
-                        value = r._ContentLength;
+                        value = r.CacheControl;
                         return true;
                     }
                 }
-                return false;
+                break;
 
             default:
-                return false;
+                break;
         }
+        value = StringValues.Empty;
+        return false;
     }
 
     private struct HeaderReferences
     {
-        public StringValues _ContentLength;
-        public StringValues _Accept;
-        public StringValues _Connection;
-        public StringValues _Host;
-        public StringValues _UserAgent;
-        public StringValues _Authority;
-        public StringValues _Method;
-        public StringValues _Path;
-        public StringValues _Protocol;
-        public StringValues _Scheme;
-        public StringValues _AcceptCharset;
-        public StringValues _AcceptEncoding;
-        public StringValues _AcceptLanguage;
-        public StringValues _AccessControlRequestHeaders;
-        public StringValues _AccessControlRequestMethod;
-        public StringValues _AltUsed;
-        public StringValues _Authorization;
-        public StringValues _Baggage;
-        public StringValues _CacheControl;
-        public StringValues _ContentType;
-        public StringValues _Cookie;
-        public StringValues _CorrelationContext;
-        public StringValues _Date;
-        public StringValues _Expect;
-        public StringValues _From;
-        public StringValues _GrpcAcceptEncoding;
-        public StringValues _GrpcEncoding;
-        public StringValues _GrpcTimeout;
-        public StringValues _IfMatch;
-        public StringValues _IfModifiedSince;
-        public StringValues _IfNoneMatch;
-        public StringValues _IfRange;
-        public StringValues _IfUnmodifiedSince;
-        public StringValues _KeepAlive;
-        public StringValues _MaxForwards;
-        public StringValues _Origin;
-        public StringValues _Pragma;
-        public StringValues _ProxyAuthorization;
-        public StringValues _Range;
-        public StringValues _Referer;
-        public StringValues _RequestId;
-        public StringValues _TE;
-        public StringValues _TraceParent;
-        public StringValues _TraceState;
-        public StringValues _TransferEncoding;
-        public StringValues _Translate;
-        public StringValues _Upgrade;
-        public StringValues _UpgradeInsecureRequests;
-        public StringValues _Via;
-        public StringValues _Warning;
+        public StringValues Accept;
+        public StringValues Connection;
+        public StringValues Host;
+        public StringValues UserAgent;
+        public StringValues Authority;
+        public StringValues Method;
+        public StringValues Path;
+        public StringValues Protocol;
+        public StringValues Scheme;
+        public StringValues AcceptCharset;
+        public StringValues AcceptEncoding;
+        public StringValues AcceptLanguage;
+        public StringValues AccessControlRequestHeaders;
+        public StringValues AccessControlRequestMethod;
+        public StringValues AltUsed;
+        public StringValues Authorization;
+        public StringValues Baggage;
+        public StringValues CacheControl;
+        public StringValues ContentType;
+        public StringValues Cookie;
+        public StringValues CorrelationContext;
+        public StringValues Date;
+        public StringValues Expect;
+        public StringValues From;
+        public StringValues GrpcAcceptEncoding;
+        public StringValues GrpcEncoding;
+        public StringValues GrpcTimeout;
+        public StringValues IfMatch;
+        public StringValues IfModifiedSince;
+        public StringValues IfNoneMatch;
+        public StringValues IfRange;
+        public StringValues IfUnmodifiedSince;
+        public StringValues KeepAlive;
+        public StringValues MaxForwards;
+        public StringValues Origin;
+        public StringValues Pragma;
+        public StringValues ProxyAuthorization;
+        public StringValues Range;
+        public StringValues Referer;
+        public StringValues RequestId;
+        public StringValues TE;
+        public StringValues TraceParent;
+        public StringValues TraceState;
+        public StringValues TransferEncoding;
+        public StringValues Translate;
+        public StringValues Upgrade;
+        public StringValues UpgradeInsecureRequests;
+        public StringValues Via;
+        public StringValues Warning;
     }
 }
